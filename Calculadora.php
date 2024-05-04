323c60282e5b9b87c867e0014cae2cca3682d76e
Juan Ruiz <juan.guerra@galileo.edu.gt>
2024-05-04 07:19:04 +0000

    initial commit

--- Calculadora.php
+++ Calculadora.php
@@ -14,9 +14,9 @@ class Calculadora {
     }
 
     public function dividir($num1, $num2) {
-        if ($num2 == 0) {
-            return "Error: No se puede dividir por cero";
-        }
+        //if ($num2 == 0) {
+        //    return "Error: No se puede dividir por cero";
+        //}
         return $num1 / $num2;
     }
 
